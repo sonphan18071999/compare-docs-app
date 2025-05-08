bdc67ee3c999706a4c21aa45baae765a9333edaf
son <43442285+sonphan18071999@users.noreply.github.com>
2025-05-08 14:02:47 +0700

    Implemented init files

--- src/app/document-comparison/document-comparison.component.ts
+++ src/app/document-comparison/document-comparison.component.ts
@@ -9,6 +9,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import * as monaco from 'monaco-editor';
 import { SafeHtmlPipe } from '../safe-html.pipe';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 interface DiffChange {
   value: string;
@@ -22,295 +23,23 @@ interface DiffChange {
   styleUrls: ['./document-comparison.component.scss'],
   standalone: true,
   providers: [SafeHtmlPipe],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
 })
 export class DocumentComparisonComponent implements OnInit, AfterViewInit {
   @ViewChild('editorContainer', { static: true }) editorContainer!: ElementRef;
 
   private editor: monaco.editor.IStandaloneDiffEditor | null = null;
+  private originalFile: File | null = null;
+  private modifiedFile: File | null = null;
 
-  public originalHTML = `<html>
-
-<head>
-<meta http-equiv=Content-Type content="text/html; charset=utf-8">
-<meta name=Generator content="Microsoft Word 15 (filtered)">
-<style>
-<!--
- /* Font Definitions */
- @font-face
-	{font-family:Wingdings;
-	panose-1:5 0 0 0 0 0 0 0 0 0;}
-@font-face
-	{font-family:"Cambria Math";
-	panose-1:2 4 5 3 5 4 6 3 2 4;}
-@font-face
-	{font-family:Aptos;
-	panose-1:2 11 0 4 2 2 2 2 2 4;}
- /* Style Definitions */
- p.MsoNormal, li.MsoNormal, div.MsoNormal
-	{margin-top:0in;
-	margin-right:0in;
-	margin-bottom:8.0pt;
-	margin-left:0in;
-	line-height:115%;
-	font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-p.MsoListParagraph, li.MsoListParagraph, div.MsoListParagraph
-	{margin-top:0in;
-	margin-right:0in;
-	margin-bottom:8.0pt;
-	margin-left:.5in;
-	line-height:115%;
-	font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-p.MsoListParagraphCxSpFirst, li.MsoListParagraphCxSpFirst, div.MsoListParagraphCxSpFirst
-	{margin-top:0in;
-	margin-right:0in;
-	margin-bottom:0in;
-	margin-left:.5in;
-	line-height:115%;
-	font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-p.MsoListParagraphCxSpMiddle, li.MsoListParagraphCxSpMiddle, div.MsoListParagraphCxSpMiddle
-	{margin-top:0in;
-	margin-right:0in;
-	margin-bottom:0in;
-	margin-left:.5in;
-	line-height:115%;
-	font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-p.MsoListParagraphCxSpLast, li.MsoListParagraphCxSpLast, div.MsoListParagraphCxSpLast
-	{margin-top:0in;
-	margin-right:0in;
-	margin-bottom:8.0pt;
-	margin-left:.5in;
-	line-height:115%;
-	font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-.MsoChpDefault
-	{font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-.MsoPapDefault
-	{margin-bottom:8.0pt;
-	line-height:115%;}
-@page WordSection1
-	{size:8.5in 11.0in;
-	margin:1.0in 1.0in 1.0in 1.0in;}
-div.WordSection1
-	{page:WordSection1;}
- /* List Definitions */
- ol
-	{margin-bottom:0in;}
-ul
-	{margin-bottom:0in;}
--->
-</style>
-
-</head>
-
-<body lang=EN-US style='word-wrap:break-word'>
-
-<div class=WordSection1>
-
-<p class=MsoNormal><span style='font-family:"Courier New"'>Bbaananna</span></p>
-
-<p class=MsoNormal><b><span style='font-family:"Courier New"'>Banananaboi dam </span></b></p>
-
-<p class=MsoNormal><b><i><span style='font-family:"Courier New"'>Catcu</span></i></b></p>
-
-<p class=MsoNormal><b><i><u><span style='font-family:"Courier New"'>Mothaiba</span></u></i></b></p>
-
-<p class=MsoListParagraph style='text-indent:-.25in'><span style='font-family:
-Symbol'>·<span style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-</span></span><b><i><u><span style='font-family:"Courier New"'>NguyenSon</span></u></i></b></p>
-
-<table class=MsoTableGrid border=1 cellspacing=0 cellpadding=0
- style='border-collapse:collapse;border:none'>
- <tr>
-  <td width=208 valign=top style='width:155.8pt;border:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>1</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border:solid windowtext 1.0pt;
-  border-left:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>3</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border:solid windowtext 1.0pt;
-  border-left:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>4</span></p>
-  </td>
- </tr>
- <tr>
-  <td width=208 valign=top style='width:155.8pt;border:solid windowtext 1.0pt;
-  border-top:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
- </tr>
- <tr>
-  <td width=208 valign=top style='width:155.8pt;border:solid windowtext 1.0pt;
-  border-top:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
- </tr>
-</table>
-
-<p class=MsoNormal><span style='font-family:"Courier New"'>&nbsp;</span></p>
-
-</div>
-
-</body>
-
-</html>
-
-  `;
-
-  public modifiedHTML = `<html>
-
-<head>
-<meta http-equiv=Content-Type content="text/html; charset=utf-8">
-<meta name=Generator content="Microsoft Word 15 (filtered)">
-<style>
-<!--
- /* Font Definitions */
- @font-face
-	{font-family:"Cambria Math";
-	panose-1:2 4 5 3 5 4 6 3 2 4;}
-@font-face
-	{font-family:Aptos;
-	panose-1:2 11 0 4 2 2 2 2 2 4;}
- /* Style Definitions */
- p.MsoNormal, li.MsoNormal, div.MsoNormal
-	{margin-top:0in;
-	margin-right:0in;
-	margin-bottom:8.0pt;
-	margin-left:0in;
-	line-height:115%;
-	font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-.MsoChpDefault
-	{font-size:12.0pt;
-	font-family:"Aptos",sans-serif;}
-.MsoPapDefault
-	{margin-bottom:8.0pt;
-	line-height:115%;}
-@page WordSection1
-	{size:8.5in 11.0in;
-	margin:1.0in 1.0in 1.0in 1.0in;}
-div.WordSection1
-	{page:WordSection1;}
--->
-</style>
-
-</head>
-
-<body lang=EN-US style='word-wrap:break-word'>
-
-<div class=WordSection1>
-
-<p class=MsoNormal><span style='font-family:"Courier New"'>Adspf </span></p>
-
-<table class=MsoTableGrid border=1 cellspacing=0 cellpadding=0
- style='border-collapse:collapse;border:none'>
- <tr>
-  <td width=208 valign=top style='width:155.8pt;border:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>1</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border:solid windowtext 1.0pt;
-  border-left:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>3</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border:solid windowtext 1.0pt;
-  border-left:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>4</span></p>
-  </td>
- </tr>
- <tr>
-  <td width=208 valign=top style='width:155.8pt;border:solid windowtext 1.0pt;
-  border-top:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
- </tr>
- <tr>
-  <td width=208 valign=top style='width:155.8pt;border:solid windowtext 1.0pt;
-  border-top:none;padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>abc</span></p>
-  </td>
-  <td width=208 valign=top style='width:155.85pt;border-top:none;border-left:
-  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  padding:0in 5.4pt 0in 5.4pt'>
-  <p class=MsoNormal style='margin-bottom:0in;line-height:normal'><span
-  style='font-family:"Courier New"'>banana</span></p>
-  </td>
- </tr>
-</table>
-
-<p class=MsoNormal><span style='font-family:"Courier New"'>&nbsp;</span></p>
-
-</div>
-
-</body>
-
-</html>
-
-  `;
-
+  public originalFileName: string = '';
+  public modifiedFileName: string = '';
+  public originalHTML = '';
+  public modifiedHTML = '';
   public originalPreview: string = '';
   public modifiedPreview: string = '';
 
-  getCleanText(html: string): string {
+  public getCleanText(html: string): string {
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
 
@@ -373,7 +102,7 @@ div.WordSection1
     return result;
   }
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.originalPreview = this.originalHTML;
@@ -417,6 +146,63 @@ div.WordSection1
     );
 
     // Set models
+    this.editor.setModel({
+      original: originalModel,
+      modified: modifiedModel,
+    });
+  }
+
+  onFileChange(event: Event, type: 'original' | 'modified'): void {
+    const input = event.target as HTMLInputElement;
+    if (!input.files?.length) return;
+
+    const file = input.files[0];
+    if (type === 'original') {
+      this.originalFile = file;
+      this.originalFileName = file.name;
+    } else {
+      this.modifiedFile = file;
+      this.modifiedFileName = file.name;
+    }
+  }
+
+  compareFiles(): void {
+    if (!this.originalFile || !this.modifiedFile) {
+      alert('Please select both files to compare');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('originalFile', this.originalFile);
+    formData.append('modifiedFile', this.modifiedFile);
+
+    this.http.post('/api/compare', formData).subscribe({
+      next: (response: any) => {
+        this.originalHTML = response.originalContent;
+        this.modifiedHTML = response.modifiedContent;
+        this.originalPreview = response.originalContent;
+        this.modifiedPreview = response.modifiedContent;
+        this.updateEditor();
+      },
+      error: (error) => {
+        console.error('Error comparing files:', error);
+        alert('Error comparing files. Please try again.');
+      },
+    });
+  }
+
+  private updateEditor(): void {
+    if (!this.editor) return;
+
+    const originalModel = monaco.editor.createModel(
+      this.getCleanText(this.originalHTML),
+      'text'
+    );
+    const modifiedModel = monaco.editor.createModel(
+      this.getCleanText(this.modifiedHTML),
+      'text'
+    );
+
     this.editor.setModel({
       original: originalModel,
       modified: modifiedModel,
